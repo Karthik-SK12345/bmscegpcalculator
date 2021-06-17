d04bbeeff452b57d7b61b09cc5163135596a332f
KARTHIK S <71306768+Karthik-SK12345@users.noreply.github.com>
2021-06-17 18:05:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,5 +22,5 @@ app.use(emailRoutes.routes);
 
 
 
-const port = process.env.PORT || '5000';
-app.listen(port, () => console.log(`Server started on Port ${port}`));
+ const PORT = process.env.PORT || 4000
+  app.listen(PORT);
